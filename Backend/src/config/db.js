1008baab358babda8f77b83d6221be3309cfb26e
Neha Sen <neha712513@gmail.com>
2022-06-08 09:59:07 +0530

    i

---
+++ Backend/src/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connect = mongoose.connect(
+    "mongodb+srv://neha:nehasen@cluster0.kscegai.mongodb.net/?retryWrites=true&w=majority"
+)
+
+module.exports = connect;
